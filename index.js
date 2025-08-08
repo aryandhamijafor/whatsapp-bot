d82cf572656183beab08aadf566afc690c7edf1b
aryandhamijafor <aryandhamija@gmail.com>
2025-08-08 14:21:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ app.get("/", (req, res) => {
   res.send("WhatsApp bot is running 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
